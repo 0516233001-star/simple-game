ac8b2ccf638fd78e8fe06157d1e7efcc941342f3
0516233001-star <0516233001@s.tusd1.org>
2026-04-06 21:58:09 +0000

    remove squares

--- script.js
+++ script.js
@@ -2,39 +2,51 @@ console.log("Script started");
 
 // Start the game
 function start() {
-    createSquare(50);
-    createSquare(80);
-    createSquare(100);
+    createSquare(50, "blue");
+    createSquare(80, "red");
+    createSquare(90, "white");
+    createSquare(110, "green");
     console.log(Math.random() + 1000);
 }
 
 // Create a square
-function createSquare(size) {
+function createSquare(size, color) {
     // Create an element (div)
     let square = document.createElement("div");
 
     // Set some styles to make it a square
-    square.style.background = "white";
+    square.style.background = color;
     square.style.width = size + "px";
     square.style.height = size + "px";
     square.style.position = "absolute";
-    square.style.boderRadius = "16px";
+    square.style.borderRadius = "16px";
     square.style.top = getRandomY() + "px";
     square.style.left = getRandomX() + "px";
 
     // Add it to the page
     document.body.appendChild(square);
+
+    // Add click event
+    square.addEventListener("click", handleClick);
 }
 
 // Generate random x cordinate 
 function getRandomX() {
-    let randX = Math.random() = window.innerWidth;
+    let randX = Math.random() * window.innerWidth;
     return randX;
 }
 
 // Generate a random y codinate
 function getRandomY() {
-    let randY = Math.random() = window.innerHeight;
+    let randY = Math.random() * window.innerHeight;
     return randY;
 
+}
+
+// Handle the player clicking a square
+function handleClick(event) {
+    let square = event.target;
+    console.log(square);
+    square.remove();
+    // Make a size pattern i.e. increase by same amount each time 10, 20, 30, 40, 50, 
 }
